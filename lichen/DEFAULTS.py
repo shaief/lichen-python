d297c47bc34cd21e980c65fe622aad57f88574a1
shaief <shaief@gmail.com>
2013-12-14 12:37:30 +0200

    aaranging directories

---
+++ lichen/DEFAULTS.py
@@ -0,0 +1,9 @@
+# this file conatins default parameters for Lichen.py
+
+DEFAULT_LOW = 150
+
+DEFAULT_HIGH = 200
+
+DEFAULT_DIRECTORY = "/home/shaief/Programming/Lichen/"
+
+DEFAULT_IMAGE = "Lichen.jpg"
